d840c8d42ef199a744095f008a92869cc0d2b792
satish5626 <yarrabathulasatish777@gmail.com>
2026-02-20 10:51:32 +0530

    Spring Boot app created

---
+++ src/main/java/com/satish/cloudapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.satish.cloudapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Satish – Cloud Project Running 🚀";
+    }
+}
